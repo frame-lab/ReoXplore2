d716bb28869d54a1a4fb6ca9304bb0425dafc2dc
Mariana Ferreira <ferreira_mariana@id.uff.br>
2021-03-20 17:43:55 -0300

    allows to create channels between nodes that already exist

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let count = 1
-
+let previous_node
+let first_node = true
 let channelMode
 
 let nodes = []
-
 let channels = []
 
 function setup() {
@@ -25,20 +25,41 @@ function draw() {
 }
 
 function main() {
+    let node
+    let node_clicked = false
+    let new_node_created = false
 
-    let node = new Node(mouseX, mouseY, count)
-    nodes.push(node);
-
-    if (count % 2 == 0) {
-        let previous_node = nodes[count-2]
-        let channel = new Channel(previous_node, node, channelMode)
-        channels.push(channel)
+    for (let i = 0; i < nodes.length; i++) { //checks if a node was clicked
+        if (nodes[i].clicked(mouseX, mouseY)){
+            if (first_node) {
+                previous_node = nodes[i]
+            } else {
+                node = nodes[i]
+            }
+            node_clicked = true
+        }
+    }
+    if (!node_clicked) { //create new node
+        node = new Node(mouseX, mouseY, count)
+        nodes.push(node);
+        new_node_created = true
     }
 
-    count++;
+    if (!first_node) {
+        if (!previous_node) { //if didnt click on a previous node, get the last one
+            previous_node = nodes[count-2]
+        }
+        let channel = new Channel(previous_node, node, channelMode)
+        channels.push(channel)
+        previous_node = null
+    }
+
+    if (new_node_created) count++;
+    first_node = !first_node
 }
 
 function chooseChannel(mode) {
     //call by button in html
     channelMode = mode
-  }
+}
+
--- node.js
+++ node.js
@@ -3,14 +3,23 @@ class Node {
     constructor(x, y, label) {
         this.x = x
         this.y = y
-        this.label = label    
+        this.label = label
+        this.radius = 20
     }
 
     draw() {
         noStroke()
         fill(255)
-        ellipse(this.x, this.y, 20, 20)
+        ellipse(this.x, this.y, this.radius, this.radius)
         fill(10)
         text(this.label, this.x - 5, this.y + 4)
     }
+
+    clicked(px, py) {
+        let d = dist(px, py, this.x, this.y)
+        if (d <= this.radius ) {
+            return true
+        }
+    }
 }
+
