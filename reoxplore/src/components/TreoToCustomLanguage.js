0c9052a92af90776c69ae2360390cb0bbd2d4a3a
Mariana Ferreira <ferreira_mariana@id.uff.br>
2021-11-10 23:31:27 -0300

    ✨ download generated code button

--- reoxplore/src/components/TreoToCustomLanguage.js
+++ reoxplore/src/components/TreoToCustomLanguage.js
@@ -1,15 +1,20 @@
 import React from "react";
 import makeRequest from "../utils/makeRequest";
-import parseTreoToHaskellInput from "../utils/parseTreoToHaskellInput"
+import parseTreoToHaskellInput from "../utils/parseTreoToHaskellInput";
 
 class TreoToCustomLanguage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { resultTitle: "", resultCode: "", resultError: "" };
+    this.state = {
+      resultTitle: "",
+      resultCode: "",
+      resultError: "",
+      resultFileName: "",
+    };
     this.handleClick = this.handleClick.bind(this);
   }
 
-  async handleClick(treo, path, title) {
+  async handleClick(treo, path, title, filename) {
     if (!treo) {
       this.setState({ resultError: "Error. Treo is empty." });
       return;
@@ -25,23 +30,25 @@ class TreoToCustomLanguage extends React.Component {
         resultCode: response.data,
         resultTitle: title,
         resultError: "",
+        resultFileName: filename,
       });
     else
       this.setState({
         resultError: "Error. Please verify if treo is correct.",
       });
-    console.log(response);
   }
 
-  renderButton = (text, path, title) => {
+  renderButton = (text, path, title, filename) => {
     const treo =
       path === "/haskell/model"
         ? parseTreoToHaskellInput(this.props.treo)
         : this.props.treo;
     return (
       <button
+        type="button"
         onClick={(e) => {
-          this.handleClick(treo, path, title, e);
+          e.preventDefault();
+          this.handleClick(treo, path, title, filename, e);
         }}
       >
         {text}
@@ -49,29 +56,43 @@ class TreoToCustomLanguage extends React.Component {
     );
   };
 
+  handleDownload = (fileData, fileName) => (e) => {
+    e.preventDefault();
+    const link = document.createElement("a");
+    link.download = fileName;
+    const blob = new Blob([fileData], { type: "text/plain" });
+    link.href = URL.createObjectURL(blob);
+    link.click();
+    URL.revokeObjectURL(link.href);
+  };
+
   render() {
     return (
-      <div className="options-container">
+      <section className="options-container">
         <div>
           {this.renderButton(
             "Generate nuXmv compact code",
             "/nuXmv/compact",
-            "nuXmv compact"
+            "nuXmv compact",
+            "nuXmvCompact.smv"
           )}
           {this.renderButton(
             "Generate nuXmv components code",
             "/nuXmv/components",
-            "nuXmv components"
+            "nuXmv components",
+            "nuXmvComponents.smv"
           )}
           {this.renderButton(
             "Generate Coq model",
             "/coq/model",
-            "Coq model"
+            "Coq model",
+            "coqModel.v"
           )}
           {this.renderButton(
             "Generate Haskell code",
             "/haskell/model",
-            "Haskell code"
+            "Haskell code",
+            "haskellModel.hs"
           )}
         </div>
         {this.state.resultError ? (
@@ -80,7 +101,18 @@ class TreoToCustomLanguage extends React.Component {
           <div className="result-container">
             {this.state.resultCode && (
               <div>
-                <h4>{this.state.resultTitle}</h4>
+                <div className="result-header">
+                  <h4>{this.state.resultTitle}</h4>
+                  <button
+                    type="button"
+                    onClick={this.handleDownload(
+                      this.state.resultCode,
+                      this.state.resultFileName
+                    )}
+                  >
+                    download
+                  </button>
+                </div>
                 <textarea
                   readOnly
                   cols="80"
@@ -91,7 +123,7 @@ class TreoToCustomLanguage extends React.Component {
             )}
           </div>
         )}
-      </div>
+      </section>
     );
   }
 }
