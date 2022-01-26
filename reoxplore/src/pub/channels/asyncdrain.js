b5a0b884842a718a95fa8ad08f98ca2de8e942a1
Mariana Ferreira <ferreira_mariana@id.uff.br>
2022-01-25 22:19:08 -0300

    ♻️ remove unused variable from asyncdrain channel

--- reoxplore/src/pub/channels/asyncdrain.js
+++ reoxplore/src/pub/channels/asyncdrain.js
@@ -4,10 +4,6 @@ import triangle from "./shapes/triangle";
 
 /** draws asyncdrain channel */
 export function asyncdrain(p5, startNode, endNode, arrowSize) {
-  const angle = p5.atan2(startNode.y - endNode.y, startNode.x - endNode.x); //angle of the line
-  const mediumX = (startNode.x + endNode.x) / 2;
-  const mediumY = (startNode.y + endNode.y) / 2;
-
   line(p5, startNode, endNode);
 
   center(p5, startNode, endNode, ["line", "line"]);
